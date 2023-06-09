87305efcf4eb2db2a40251785c06a794977da27e
kmc77 <k_minchoi@naver.com>
2023-06-09 15:01:53 +0900

    동기화 메서드 예

---
+++ src/ex17_11_syn_format2/MultithreadExample2.java
@@ -0,0 +1,14 @@
+package ex17_11_syn_format2;
+
+//동기화 메서드 예
+import java.util.ArrayList;
+import java.util.Vector;
+
+class MultithreadExample2 {
+
+	public static void main(String[] args) {
+		Vector<String> vec = new Vector<String>();
+		ArrayList<String> list = new ArrayList();
+	}
+
+}
