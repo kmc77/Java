3dcc4689912020f0bedc2a134f59453b4170d9fa
kmc77 <k_minchoi@naver.com>
2023-06-09 11:09:30 +0900

    파이를 출력하는 스레드 클래스

---
+++ src/ex17_06_SharedArea_problem/PrintThread.java
@@ -0,0 +1,16 @@
+package ex17_06_SharedArea_problem;
+
+//파이를 출력하는 스레드 클래스
+class PrintThread extends Thread {
+
+	private SharedArea sharedArea;
+
+	PrintThread(SharedArea sharedArea) {
+		this.sharedArea = sharedArea;
+	}
+
+	public void run() {
+		System.out.println("공유 영역의 데이터 = " + sharedArea.getResult()); // 공유 영역의 데이터 출력합니다.
+	}
+
+}
