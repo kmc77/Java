5fef7e2ded3853e325b3de870c2f3fa36f328dab
kmc77 <k_minchoi@naver.com>
2023-06-09 15:26:50 +0900

    notify메서드와 wait메서드 사용 예

--- src/ex17_12_notify_wait/MultithreadExample.java
+++ src/ex17_12_notify_wait/MultithreadExample.java
@@ -1,9 +1,13 @@
 package ex17_12_notify_wait;
 
-//CalcThread 가 계산된 파이 값을 공유 영역에 쓰기도 전에
-//PrintThread가 공유 영역에 데이터를 읽어 갔기 때문에
-//result 필드가 가지고 있던 기본깞 0.0이 출력된 것입니다.
-//해결책?
+//notify메서드와 wait메서드 사용 예
+/*
+ 	- 스레드 사이에 신호를 직접 주고 받을 수 있습니다.
+ 	동기화된 스레드는 동기화 블록에서 다른 스레드에게 제어권을 넘기지 못합니다.
+ 	이와 같이 동기화된 블록에서 스레드간의 통신(제어권을 넘김) 하기 위해서
+ 	wait(), notify(), notifyAll() 메서드를 사용해야 합니다.
+ 	이 메서드를 사용할 때는 synchronized 블록에서만 의미가 있습니다.
+ */
 public class MultithreadExample {
 
 	public static void main(String[] args) {
