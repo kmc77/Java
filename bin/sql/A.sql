14c7ffa655fb64fd991499f5fc2976e3e42ff9c2
kmc77 <k_minchoi@naver.com>
2023-06-19 18:11:40 +0900

    Student3 + CRUD3 + DAO3 예제
    출력결과)
    ===========================================================
    1.입력
    2.조회
    3.수정
    4.삭제
    5.모두조회
    6.종료
    ===========================================================
    메뉴를 선택하세요>1
    이름을 입력하세요>이 광수
    국어점수를 입력하세요>96
    수학점수를 입력하세요>85
    영어점수를 입력하세요>93
    db에 반영됨 . . . . . .
    삽입 되었습니다.

--- bin/sql/A.sql
+++ bin/sql/A.sql
@@ -33,15 +33,85 @@ insert into goodsinfo
  
  select * from dept;
  
+ drop table hakjum cascade constraints;
  
-create table test(
-	no number(10),
-	name varchar2(20)
+-- 1. hakjum 테이블 생성합니다.	
+create table hakjum(
+	LOWSCORE     NUMBER(3),
+	HISCORE      NUMBER(5,2),
+	GRADE        VARCHAR2(3) primary key  -- 기본키
+	);
+	
+	
+insert into hakjum 
+ VALUES(0, 59.99, 'F');
+ 
+ insert into hakjum 
+ VALUES(60, 64.99, 'D0');
+ 
+ insert into hakjum 
+ VALUES(65, 69.99, 'D+');
+ 
+ insert into hakjum 
+ VALUES(70, 74.99, 'C0');
+ 
+ insert into hakjum 
+ VALUES(75, 79.99, 'C+');
+ 
+ insert into hakjum 
+ VALUES(80, 84.99, 'B0');
+ 
+ insert into hakjum 
+ VALUES(85, 89.99, 'B+');
+ 
+ insert into hakjum 
+ VALUES(90, 94.99, 'A0');
+ 
+ insert into hakjum 
+ VALUES(95, 100, 'A+');
+ 
+ delect from HAKJUM;
+ select * from hakjum; --데이터 확인합니다.
+ 
+ --2. student 테이블 생성합니다.
+ drop table student purge;
+ create table student(
+	no number primary key, -- 기본키
+	name varchar2(21) not null,
+	kor number(3) check(kor between 0 and 100) not null, -- 0~100 사이의 값, not null
+	math number(3) check(math between 0 and 100) not null, -- 0~100 사이의 값, not null
+	eng number(3) check(eng between 0 and 100) not null,  -- 0~100 사이의 값, not null
+	tot number(3),
+	avg number(5,2),
+	grade varchar2(3) references hakjum(grade)
 	);
 
- 
+ select * from student --데이터 확인합니다.
+ delete from student;
+
+--3. 시퀀스 생성합니다.
+drop sequence student_seq;
+
+create sequence student_seq;
+
+-- 4. 더미 데이터를 이용한 insert 쿼리문 작성해 봅시다.
+--- 1) 평균이 100점인 경우 grade를 구해 봅시다.
+select GRADE
+from HAKJUM
+where 100 between lowscore and hiscore;
 
 
+--- 2) 위에서 사용한 쿼리문을 insert 문에 사용합니다.
+insert into STUDENT
+values(1, '홍길동', 100,100,100,300,100,
+		(select grade from HAKJUM where 100 between lowscore and hiscore)
+		);
+		
+insert into STUDENT
+values(시퀀스, ? , ?,?,?,?,?,
+		(select grade from HAKJUM where ? between lowscore and hiscore)
+		);
  
+select * from student;		
  
 	
